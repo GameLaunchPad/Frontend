14ab71fe46d0daa9cd65fb2d68f8c39bff6c2333
Integral <integral@member.fsf.org>
2025-10-14 23:28:19 +0800

    fix: change the href property of GameLaunchPad Typography to /

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -93,7 +93,7 @@ export function NavBar(): React.JSX.Element {
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
