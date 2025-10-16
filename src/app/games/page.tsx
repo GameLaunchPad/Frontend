69b71da2ff66031068a2244f311f667003f3546f
Integral <integral@member.fsf.org>
2025-10-16 23:37:09 +0800

    feat: implement game card in the game dashboard

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -16,83 +16,86 @@ import {
   Typography,
   ButtonGroup,
   Button,
+  CardMedia,
+  CardActions,
+  Chip,
 } from '@mui/material';
 
 export default function GamePad() {
   return (
     <Box sx={{ display: 'flex' }}>
-      <EdgeDrawer/>
-      <GameDashboard/>
+      <EdgeDrawer />
+      <GameDashboard />
     </Box>
   );
 }
 
 function EdgeDrawer() {
-    const drawerWidth = 240;
+  const drawerWidth = 240;
 
-    return (
-      <Drawer
-        variant="permanent"
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
-        }}
-      >
-        <Toolbar />
-        <Box sx={{ overflow: 'auto' }}>
-          <List>
-            {['My Games', 'New Game', 'Review Queue'].map((text) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            <ListItem disablePadding>
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+      }}
+    >
+      <Toolbar />
+      <Box sx={{ overflow: 'auto' }}>
+        <List>
+          {['My Games', 'New Game', 'Review Queue'].map((text) => (
+            <ListItem key={text} disablePadding>
               <ListItemButton>
-                <ListItemText primary="Provider Management" />
+                <ListItemText primary={text} />
               </ListItemButton>
             </ListItem>
-            <ListItem disablePadding>
-              <ListItemButton href='/cp-info'>
-                <ListItemText primary="Provider Information" />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Provider Management" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton href='/cp-info'>
+              <ListItemText primary="Provider Information" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Certificate Management" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          {['Data Analysis', 'Download Statistics', 'User Feedbacks'].map((text) => (
+            <ListItem key={text} disablePadding>
               <ListItemButton>
-                <ListItemText primary="Certificate Management" />
+                <ListItemText primary={text} />
               </ListItemButton>
             </ListItem>
-          </List>
-          <Divider />
-          <List>
-            {['Data Analysis', 'Download Statistics', 'User Feedbacks'].map((text) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-      </Drawer>
-    );
+          ))}
+        </List>
+      </Box>
+    </Drawer>
+  );
 }
 
 function GameDashboard() {
   return (
-   <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
       <Typography variant="h3" gutterBottom>
         My Games
       </Typography>
       <Typography variant="h5" gutterBottom>
         Game Management &gt; My Games
       </Typography>
-      <Divider/>
+      <Divider />
       <Box mt={4}>
         <Grid container spacing={2} justifyContent="center">
           <Card sx={{ flex: 1 }}>
@@ -100,7 +103,7 @@ function GameDashboard() {
               <Typography variant="h5" component="div">
                 N/A
               </Typography>
-              <Typography variant="body1" color="text.secondary">
+              <Typography variant="subtitle1" color="text.secondary">
                 Total
               </Typography>
             </CardContent>
@@ -110,7 +113,7 @@ function GameDashboard() {
               <Typography variant="h5" component="div">
                 N/A
               </Typography>
-              <Typography variant="body1" color="text.secondary">
+              <Typography variant="subtitle1" color="text.secondary">
                 Published
               </Typography>
             </CardContent>
@@ -120,8 +123,8 @@ function GameDashboard() {
               <Typography variant="h5" component="div">
                 N/A
               </Typography>
-              <Typography variant="body1" color="text.secondary">
-                Reviewing 
+              <Typography variant="subtitle1" color="text.secondary">
+                Reviewing
               </Typography>
             </CardContent>
           </Card>
@@ -162,6 +165,63 @@ function GameDashboard() {
           </Grid>
         </Grid>
       </Box>
+      <Box mt={4}>
+        <Grid container spacing={2} columns={3}>
+          <GameCard />
+        </Grid>
+      </Box>
     </Box>
+  );
+}
+
+function GameCard() {
+  return (
+    <Card sx={{ minWidth: 400, paddingLeft: 2, paddingRight: 2 }}>
+      <CardMedia sx={{ height: 140 }} />
+      <Chip label="Released" color="success" sx={{ position: 'sticky', bottom: '90%', left: '100%', zIndex: '1' }} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Game Name
+        </Typography>
+      </CardContent>
+      <Chip label="Android" />
+      <Grid mt={2} container spacing={2} justifyContent="center">
+        <Card sx={{ flex: 1.5 }}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              N/A
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary">
+              Downloads
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card sx={{ flex: 1.5 }}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              N/A
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary">
+              Rating
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card sx={{ flex: 1.5 }}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              N/A
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary">
+              Version
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <CardActions sx={{ justifyContent: 'center' }}>
+        <Button>View</Button>
+        <Button>Edit</Button>
+        <Button>Statistics</Button>
+      </CardActions>
+    </Card>
   );
 }
